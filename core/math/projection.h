60056594aa5bd13506f0b95bdec6cda2c58ecdc4
GingerCoderShaman <shamancoderginger@gmail.com>
2024-08-17 17:55:05 -0500

    * baseline needed for portal code in axiom null.

--- core/math/projection.h
+++ core/math/projection.h
@@ -78,6 +78,7 @@ struct [[nodiscard]] Projection {
 	void set_orthogonal(real_t p_left, real_t p_right, real_t p_bottom, real_t p_top, real_t p_znear, real_t p_zfar);
 	void set_orthogonal(real_t p_size, real_t p_aspect, real_t p_znear, real_t p_zfar, bool p_flip_fov = false);
 	void set_frustum(real_t p_left, real_t p_right, real_t p_bottom, real_t p_top, real_t p_near, real_t p_far);
+	void apply_oblique_plane(Vector4 p_oblique_plane);
 	void set_frustum(real_t p_size, real_t p_aspect, Vector2 p_offset, real_t p_near, real_t p_far, bool p_flip_fov = false);
 	void adjust_perspective_znear(real_t p_new_znear);
 
